35c00b83302ebeed55e43de610d9eda7984daa61
vishal-pk <54106270+vishal-pk@users.noreply.github.com>
2020-11-06 17:40:34 +0530

    message-container

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,29 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { NavbarComponent } from './navbar/navbar.component';
 
 import { SideNavComponent } from './side-nav/side-nav.component';
 
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MessageBoxComponent } from './message-box/message-box.component';
 import { MessageContainerComponent } from './message-container/message-container.component';
 import {ScrollingModule} from '@angular/cdk/scrolling';
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
-    SideNavComponent,
-    MessageBoxComponent,
-    MessageContainerComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
-    ScrollingModule
-    
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
