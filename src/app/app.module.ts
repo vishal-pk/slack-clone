0e3dcb1e845e1acfaea8932c91d3f418d9a65c91
vishal-pk <54106270+vishal-pk@users.noreply.github.com>
2020-11-07 11:31:32 +0530

    'base'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,14 @@ import {FormsModule} from   '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SideNavComponent } from './side-nav/side-nav.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatListModule} from '@angular/material/list';
 import { MessageComponent } from './message/message.component';
 import { PickerModule } from '@ctrl/ngx-emoji-mart';
 
 import { NavbarComponent } from './navbar/navbar.component';
-import { SideNavComponent } from './side-nav/side-nav.component';
 
 import { ChatHeaderComponent } from './chat-header/chat-header.component';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatListModule} from '@angular/material/list';
 import { FirstListComponent } from './first-list/first-list.component';
 import { MatIconModule } from "@angular/material/icon";
 
